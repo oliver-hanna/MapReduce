99e89c62969356c3702f856cef38531ae375b07b
oliver-hanna <oliverhanna1@gmail.com>
2023-03-07 17:22:43 -0800

    Finish concurrent MapReduce solution

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,226 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"sync"
+	"time"
+)
 
+const (
+	NotStarted = iota
+	InProgress
+	Finished
+)
 
 type Coordinator struct {
 	// Your definitions here.
+	numMapTasks int
+	nReduce     int
 
+	mapTasksInProgress    int
+	reduceTasksInProgress int
+
+	mapProgress    map[int]*MapJob
+	reduceProgress map[int]*ReduceJob
+
+	finishedMaps    int
+	finishedReduces int
+	mapFinished     bool
+	reduceFinished  bool
+
+	mux sync.Mutex
 }
 
-// Your code here -- RPC handlers for the worker to call.
+type MapJob struct {
+	FileName     string
+	Status       int
+	FinishSignal chan struct{}
+}
 
-//
-// an example RPC handler.
-//
-// the RPC argument and reply types are defined in rpc.go.
-//
-func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
-	reply.Y = args.X + 1
+type ReduceJob struct {
+	Files        []string
+	Status       int
+	FinishSignal chan struct{}
+}
+
+func (c *Coordinator) RequestTask(args *RequestTaskArgs, reply *RequestTaskReply) error {
+	// Check if there are un-run map tasks
+	if mapJobID, fileName, mapJobToRun := c.fetchMapJob(); mapJobToRun {
+		reply.TaskType = MapTask
+		reply.FileName = fileName
+		reply.NReduce = c.nReduce
+		reply.MapTaskNum = mapJobID
+		// Fire off a timer in another thread to track worker failure
+		go c.timeMapJob(mapJobID)
+		return nil
+	}
+
+	// Check if maps are still completing (and may need to be re-run)
+	if c.waitForMapFinish() {
+		// Tell worker to wait for maps to finish
+		reply.TaskType = WaitTask
+		return nil
+	}
+
+	// Check if there are un-run reduce tasks
+	if reduceID, bucket, reduceJobToRun := c.fetchReduceJob(); reduceJobToRun {
+		reply.TaskType = ReduceTask
+		reply.ReduceTaskNum = reduceID
+		reply.ReduceFiles = bucket
+		// Fire off a timer in another thread to track worker failure
+		go c.timeReduceJob(reduceID)
+		return nil
+	}
+
+	// Check if reduces are still completing (and may need to be re-run)
+	if c.waitForReduceFinish() {
+		// Tell worker to wait for reduces to finish
+		reply.TaskType = WaitTask
+		return nil
+	}
+
+	reply.TaskType = DoneTask
 	return nil
 }
 
+func (c *Coordinator) fetchMapJob() (int, string, bool) {
+	found := false
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	if c.mapFinished {
+		return -1, "", found
+	}
+	for id, job := range c.mapProgress {
+		//fmt.Println(job)
+		if job.Status == NotStarted {
+			c.mapProgress[id].Status = InProgress
+			c.mapTasksInProgress++
+			found = true
+			return id, job.FileName, found
+		}
+	}
+	return -1, "", found
+}
 
-//
+func (c *Coordinator) waitForMapFinish() bool {
+	wait := false
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	if c.mapTasksInProgress > 0 {
+		wait = true
+	}
+	return wait
+}
+
+func (c *Coordinator) fetchReduceJob() (int, []string, bool) {
+	found := false
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	for id, job := range c.reduceProgress {
+		if job.Status == NotStarted {
+			c.reduceProgress[id].Status = InProgress
+			c.reduceTasksInProgress++
+			found = true
+			return id, c.reduceProgress[id].Files, found
+		}
+	}
+	return -1, nil, found
+}
+
+func (c *Coordinator) waitForReduceFinish() bool {
+	wait := false
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	if c.reduceTasksInProgress > 0 {
+		wait = true
+	}
+	return wait
+}
+
+func (c *Coordinator) timeMapJob(id int) {
+	ticker := time.NewTicker(10 * time.Second)
+	defer ticker.Stop()
+	// This introduces data race
+	// defer close(c.mapProgress[id].FinishSignal)
+	for {
+		select {
+		case <-ticker.C:
+			c.mux.Lock()
+			defer c.mux.Unlock()
+			c.mapProgress[id].Status = NotStarted
+			c.mapTasksInProgress--
+			return
+		case <-c.mapProgress[id].FinishSignal:
+			return
+		default:
+			// Could sleep a bit here to reduce how often this loop
+			// is checking
+		}
+	}
+}
+
+func (c *Coordinator) timeReduceJob(id int) {
+	ticker := time.NewTicker(10 * time.Second)
+	defer ticker.Stop()
+	// This introduces data race
+	// defer close(c.reduceProgress[id].FinishSignal)
+	for {
+		select {
+		case <-ticker.C:
+			c.mux.Lock()
+			defer c.mux.Unlock()
+			c.reduceProgress[id].Status = NotStarted
+			c.reduceTasksInProgress--
+			return
+		case <-c.reduceProgress[id].FinishSignal:
+			return
+		default:
+			// Could sleep a bit here to reduce how often this loop
+			// is checking
+		}
+	}
+}
+
+func (c *Coordinator) FinishMap(args *MapResponseArgs, reply *MapResponseReply) error {
+	c.mux.Lock()
+	c.mapProgress[args.ID].Status = Finished
+
+	for _, bucket := range args.Buckets {
+		c.reduceProgress[bucket.ReduceNum].Files = append(c.reduceProgress[bucket.ReduceNum].Files, bucket.FileName)
+	}
+	c.mapTasksInProgress--
+	c.finishedMaps++
+	if c.finishedMaps == c.numMapTasks {
+		c.mapFinished = true
+	}
+	c.mux.Unlock()
+	c.mapProgress[args.ID].FinishSignal <- struct{}{}
+	// Since this seems to work does it mean the channel can still receive after it is closed?
+	// That is - does close block sending only?
+	// See: The Channel Closing Principle
+	// close(c.mapProgress[args.ID].FinishSignal)
+	return nil
+}
+
+func (c *Coordinator) FinishReduce(args *ReduceResponseArgs, reply *ReduceResponseReply) error {
+	c.mux.Lock()
+	c.reduceProgress[args.ReduceN].Status = Finished
+	c.reduceTasksInProgress--
+	c.finishedReduces++
+	if c.finishedReduces == c.nReduce {
+		c.reduceFinished = true
+	}
+	c.mux.Unlock()
+	c.reduceProgress[args.ReduceN].FinishSignal <- struct{}{}
+	return nil
+}
+
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,29 +234,52 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
-	ret := false
-
-	// Your code here.
-
-
-	return ret
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	if c.mapFinished && c.reduceFinished {
+		fmt.Println("DONE!")
+	}
+	return c.mapFinished && c.reduceFinished
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
-
 	// Your code here.
+	c := Coordinator{
+		numMapTasks:     len(files),
+		nReduce:         nReduce,
+		mapFinished:     false,
+		finishedMaps:    0,
+		finishedReduces: 0,
+		reduceFinished:  false,
 
+		mapTasksInProgress:    0,
+		reduceTasksInProgress: 0,
+	}
+
+	c.mapProgress = make(map[int]*MapJob)
+	c.reduceProgress = make(map[int]*ReduceJob)
+
+	for i, file := range files {
+		c.mapProgress[i] = &MapJob{
+			FileName:     file,
+			Status:       NotStarted,
+			FinishSignal: make(chan struct{}, 1),
+		}
+	}
+	for i := 0; i < nReduce; i++ {
+		c.reduceProgress[i] = &ReduceJob{
+			Status:       NotStarted,
+			FinishSignal: make(chan struct{}, 1),
+		}
+	}
+
+	fmt.Printf("num files: %d\n", len(files))
 
 	c.server()
 	return &c
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,77 +1,202 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"encoding/json"
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"net/rpc"
+	"os"
+	"time"
 
+	guuid "github.com/google/uuid"
+)
 
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
+// Worker continuously calls the coordinator for new tasks to execute until
+// the coordinator either errors or indicates there is no more work to do.
 // main/mrworker.go calls this function.
-//
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
+	// Create a unique ID for this worker, currently only used for logging purposes
+	workerID := guuid.NewString()
+	for {
+		// RPC call to get task to work on
+		task, callFailed := RequestTask()
+		// Uncomment for debugging
+		// fmt.Printf("worker-%s: taskType: %s, mNum: %d, rNum: %d\n", workerID, task.TaskType, task.MapTaskNum, task.ReduceTaskNum)
 
-	// Your worker implementation here.
-
-	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
-
-}
-
-//
-// example function to show how to make an RPC call to the coordinator.
-//
-// the RPC argument and reply types are defined in rpc.go.
-//
-func CallExample() {
-
-	// declare an argument structure.
-	args := ExampleArgs{}
-
-	// fill in the argument(s).
-	args.X = 99
-
-	// declare a reply structure.
-	reply := ExampleReply{}
-
-	// send the RPC request, wait for the reply.
-	// the "Coordinator.Example" tells the
-	// receiving server that we'd like to call
-	// the Example() method of struct Coordinator.
-	ok := call("Coordinator.Example", &args, &reply)
-	if ok {
-		// reply.Y should be 100.
-		fmt.Printf("reply.Y %v\n", reply.Y)
-	} else {
-		fmt.Printf("call failed!\n")
+		// Work may be done or there was an error, return
+		if callFailed {
+			fmt.Printf("Worker-%s: call to coordinator failed, exiting.\n", workerID)
+			return
+		}
+		switch task.TaskType {
+		case DoneTask:
+			// No more work to do, return
+			fmt.Printf("Worker-%s: all tasks finished, exiting.\n", workerID)
+			return
+		case MapTask:
+			// Handle map task
+			WorkMap(task, mapf)
+		case ReduceTask:
+			// Handle reduce task
+			WorkReduce(task, reducef)
+		case WaitTask:
+			// Let other workers finish before requesting a new task
+			time.Sleep(500 * time.Millisecond)
+		default:
+			// Error case, return
+			fmt.Printf("Worker-%s: received unknown TaskType: %s, exiting.\n", workerID, task.TaskType)
+			return
+		}
 	}
 }
 
-//
+// WorkMap maps a task using the given map function.
+func WorkMap(task RequestTaskReply, mapf func(string, string) []KeyValue) {
+	filename := task.FileName
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("cannot open %v", filename)
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", filename)
+	}
+	file.Close()
+	mapTaskNum := task.MapTaskNum
+	nReduce := task.NReduce
+
+	kva := mapf(filename, string(content))
+	buckets := make([]IntTasks, 0)
+	intFileKvs := make(map[int][]KeyValue)
+
+	// Map each keyvalue pair to a bucket
+	// Initially I tried to create/append to intermediate files on the fly
+	// save program memory by needing to not create intFileKvs,
+	// but that is ultimately much slower on the test datasets and causes
+	// inconsistencies with the total counts on my machine (unless each
+	// worker is always forced to wait after each task, which is not an
+	// acceptable fix). My guess is the inconsistencies are related to the
+	// massive amounts of various file opening/closing, but wasn't able to
+	// fully debug the issue.
+	for _, kv := range kva {
+		reduceTaskNum := ihash(kv.Key) % nReduce
+		intFileKvs[reduceTaskNum] = append(intFileKvs[reduceTaskNum], kv)
+	}
+	// Create and write to intermediate files one at a time
+	for rNum, vals := range intFileKvs {
+		intFileName := fmt.Sprintf("mr-%d-%d", mapTaskNum, rNum)
+		intTask := IntTasks{
+			FileName:  intFileName,
+			MapNum:    mapTaskNum,
+			ReduceNum: rNum,
+		}
+		buckets = append(buckets, intTask)
+		intFile, err := os.Create(intFileName)
+		if err != nil {
+			fmt.Println("error opening file, panicing")
+			panic(err)
+		}
+		enc := json.NewEncoder(intFile)
+		for _, kv := range vals {
+			err = enc.Encode(&kv)
+			if err != nil {
+				// TODO: handle error
+				fmt.Println("encoding err")
+			}
+		}
+		intFile.Close()
+	}
+	ReturnMapResponse(buckets, mapTaskNum, filename)
+}
+
+// WorkReduce reduces a task using the given reduce function.
+func WorkReduce(task RequestTaskReply, reducef func(string, []string) string) {
+	kva := make(map[string][]string)
+	for _, filename := range task.ReduceFiles {
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Fatalf("cannot open %v", filename)
+		}
+		dec := json.NewDecoder(file)
+
+		for {
+			var kv KeyValue
+			if err := dec.Decode(&kv); err != nil {
+				// TODO: This assumes err is an EOF, if it's not
+				// we should handle the different err properly
+				break
+			}
+			kva[kv.Key] = append(kva[kv.Key], kv.Value)
+		}
+		file.Close()
+	}
+	oname := fmt.Sprintf("mr-out-%d", task.ReduceTaskNum)
+	ofile, err := os.Create(oname)
+	if err != nil {
+		panic(err)
+	}
+	for key, vals := range kva {
+		output := reducef(key, vals)
+		fmt.Fprintf(ofile, "%v %v\n", key, output)
+	}
+	ofile.Close()
+	ReturnReduceResponse(task.ReduceTaskNum)
+}
+
+// RequestTask sends an RPC call to the coordinator for a new task.
+func RequestTask() (RequestTaskReply, bool) {
+	args := RequestTaskArgs{}
+	reply := RequestTaskReply{}
+	ok := call("Coordinator.RequestTask", &args, &reply)
+	if !ok {
+		fmt.Printf("Coordinator.RequestTask call failed!\n")
+		return reply, true
+	}
+	return reply, false
+}
+
+// ReturnMapResponse sends an RPC to the coordinator indicating the
+// completion of a map task.
+func ReturnMapResponse(buckets []IntTasks, id int, fileName string) {
+	args := MapResponseArgs{Buckets: buckets, ID: id}
+	reply := MapResponseReply{}
+	ok := call("Coordinator.FinishMap", &args, &reply)
+	if !ok {
+		fmt.Printf("Coordinator.FinishMap call failed!\n")
+	}
+}
+
+// ReturnReduceResponse sends an RPC to the coordinator indicating the
+// completion of a reduce task.
+func ReturnReduceResponse(reduceN int) {
+	args := ReduceResponseArgs{ReduceN: reduceN}
+	reply := ReduceResponseReply{}
+	ok := call("Coordinator.FinishReduce", &args, &reply)
+	if !ok {
+		fmt.Printf("Coordinator.FinishReduce call failed!\n")
+	}
+}
+
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
@@ -86,6 +211,6 @@ func call(rpcname string, args interface{}, reply interface{}) bool {
 		return true
 	}
 
-	fmt.Println(err)
+	fmt.Printf("call error: %s\n:", err)
 	return false
 }
